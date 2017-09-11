4d90fbe3d5124a1cf9eb35c26aafd3b3cb9b4d20
Ghanshyam Baviskar <gnbaviskar@gmail.com>
2017-09-11 19:40:24 +0530

    api

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const crypto = require('crypto').randomBytes(256).toString('hex');
 
 module.exports = {
-    uri: 'mongodb://localhost:27017/blogapp',
+    // uri: 'mongodb://localhost:27017/blogapp',
+    uri: 'mongodb://ghanshyam:ghanshyam@ds133104.mlab.com:33104/blogapp',
     secret: crypto,
     db: 'blogapp'
 }
